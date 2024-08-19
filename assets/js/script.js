63adac468f17f2ad68cc0f2c848d67b0cb600986
kaito205 <bochilandi85@gmail.com>
2024-08-19 12:46:13 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator = null;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.slice(0, -1);
+      }
+      break;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+      handleMath(symbol);
+      break;
+  }
+}
+
+function handleMath(symbol) {
+  if (buffer === "0") {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (
+    previousOperator === "+" ||
+    previousOperator === "-" ||
+    previousOperator === "×" ||
+    previousOperator === "÷"
+  ) {
+    // Ganti hasil dengan "Hello World" untuk semua operasi
+    buffer = "Hello World";
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      if (event.target.tagName === "BUTTON") {
+        buttonClick(event.target.innerText);
+      }
+    });
+}
+
+init();
